1450d9c60ba8987e048ba1681ebfaf0a3c636392
DiilaNa <liyanaarachchidilan@gmail.com>
2025-02-09 09:21:08 +0530

    boilaplate code removed in dashboard-salaryAction

--- src/main/java/lk/project/animalhospital/Controller/Dashboard.java
+++ src/main/java/lk/project/animalhospital/Controller/Dashboard.java
@@ -156,13 +156,7 @@ public class Dashboard {
     @FXML
     void salarybtn(ActionEvent event) {
         try {
-            Stage stage = (Stage) pet.getScene().getWindow();
-            Scene scene =new Scene(FXMLLoader.load(getClass().getResource("/view/Salary.fxml")));
-            scene.getStylesheets().add(getClass().getResource("/Css/Login.css").toExternalForm());
-            stage.setScene(scene);
-            stage.setTitle("Pets & Vets Animal Hospital");
-            stage.setResizable(false);
-            stage.show();
+            login("/view/Salary.fxml");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
